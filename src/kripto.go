bc90a6aca25fba110d78554da485ad61bfacdbb8
skovati <skovati@protonmail.com>
2020-08-12 22:12:22 -0500

    Initial push for v2 beta, switched to a json key value store instead of an sqlite3 db, and switched to command line arguments rather than a tui

--- src/kripto.go
+++ src/kripto.go
@@ -1,129 +1,177 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/json"
 	"fmt"
-	"log"
 	"strconv"
 	"strings"
     "net/http"
     "io/ioutil"
-	_ "github.com/mattn/go-sqlite3"
+    "os"
 )
 
+/*
+options
+---------
+init
+add
+remove
+update
+list
+help
+*/
+
+const fileName string = "portfolio.json"
+
+
 func main() {
-	db, _ := sql.Open("sqlite3", "kripto.db")
-	db.Exec("CREATE TABLE IF NOT EXISTS Portfolio (Currency TEXT, Amount FLOAT)")
-	loop(db)
+    if len(os.Args) < 2 {
+        usage()
+        return
+    }
+    switch os.Args[1] {
+    case "help":
+        usage()
+    case "init":
+        fmt.Println("Creating new portfolio...")
+        newPortfolio()
+        fmt.Println("New portfolio created, add currency using the add command.")
+    case "add", "update":
+        if len(os.Args) < 4 {
+            incorrectUsage()
+            return
+        }
+        amt, err := strconv.ParseFloat(os.Args[3], 64)
+        cur := strings.ToLower(os.Args[2])
+        if isErr(err) {
+            fmt.Println("Please enter the amount of", os.Args[2], "you have")
+        }
+        fmt.Println("Adding", amt, "of", cur, "to your portfolio...")
+        addCrypto(cur, amt)
+        fmt.Println("Succesfully added", cur, "to your portfolio, use the list command to view.")
+    case "remove":
+        if len(os.Args) < 3 {
+            incorrectUsage()
+            return
+        }
+        var response string
+        fmt.Println("Removing", os.Args[2], "from your portfolio... Are you sure? [Y/n]")
+        fmt.Scan(&response)
+        if response != "Y" {
+            fmt.Println(os.Args[2], "has not been removed.")
+            return
+        }
+        removeCrypto(strings.ToLower(os.Args[2]))
+        fmt.Println(os.Args[2], "has been removed.")
+    case "list":
+        fmt.Println("\nKripto Portfolio:\nCoin, Amount, Value, Market Price")
+        fmt.Println("-------------------------")
+        listPortfolio()
+        fmt.Println("\n")
+    default:
+        usage()
+    }
 }
 
-func checkErr(err error) {
+func usage() {
+    fmt.Println(
+        "\n",
+        "Welcome to Kripto!\n\n",
+        "commands:\n",
+        "init - create a new portfolio\n", 
+        "add - Use 'kripto add <currency> <amount>' to add a new crypto\n",
+        "remove - Use 'kripto remove <currency>' to remove\n",
+        "update - Use 'kripto update <currency>' <new amount> to update holdings\n",
+        "list - lists current porfolio and holdings\n",
+        "help - show this message\n",
+    )
+}
+
+func incorrectUsage() {
+    fmt.Println("\nIncorrect usage, use the help command.")
+    usage()
+}
+
+func newPortfolio() {
+    _, err := os.Create(fileName)
+    if isErr(err) {
+        fmt.Println("Cannot create new portfolio.")
+        return
+    }
+    return
+}
+
+func openFile() map[string]float64 {
+    portfolio := make(map[string]float64)
+    portfolioJson, err := ioutil.ReadFile(fileName)
+    if isErr(err) {
+        fmt.Println("File could not be read, try 'kripto init'")
+        return nil
+    }
+    json.Unmarshal(portfolioJson, &portfolio)
+    return portfolio
+}
+
+func savePortfolio(portfolio map[string]float64) {
+    portfolioJson, err := json.MarshalIndent(portfolio, "", "   ")
+    if isErr(err) {
+        fmt.Println("Error encoding as json.")
+    }
+    err = ioutil.WriteFile(fileName, portfolioJson, 0770)
+    if isErr(err) {
+        fmt.Println("Error writing to file.")
+    }
+    return
+}
+
+func addCrypto(currency string, amount float64) {
+    portfolio := openFile()
+    portfolio[currency] = amount
+    savePortfolio(portfolio)
+}
+
+func removeCrypto(currency string) {
+    portfolio := openFile()
+    delete(portfolio, currency)
+    savePortfolio(portfolio)
+}
+
+func listPortfolio() {
+    portfolio := openFile()
+    if (len(portfolio)) == 0 {
+        fmt.Println("Nothing to see here, add crypto using the add command.")
+        return
+    }
+    sum := 0.0
+    for s, v := range(portfolio) {
+        price := getPrice(s)
+        fmt.Printf("%s: %.3f, $%.2f, $%.2f\n", strings.Title(s), v, v*price, price)
+        sum += price*v
+    }
+    fmt.Println("-------------------------")
+    fmt.Printf("Total: $%.2f", sum)
+}
+
+func isErr(err error) bool {
 	if err != nil {
-		log.Fatal(err.Error())
+        return true
 	}
-}
-
-func addCrypto(db *sql.DB, currency string, amount float64) {
-	tx, _ := db.Begin()
-	statement, _ := tx.Prepare("INSERT INTO Portfolio (currency, amount) values (?,?)")
-	_, err := statement.Exec(currency, amount)
-	checkErr(err)
-	tx.Commit()
-}
-
-func getPortfolio(db *sql.DB) {
-    var sum float64
-	rows, _ := db.Query("SELECT * FROM Portfolio ORDER BY Amount DESC")
-	for rows.Next() {
-		var tempCur string
-		var tempAmt float64
-		rows.Scan(&tempCur, &tempAmt)
-        tempPrice := getPrice(tempCur)
-        sum += tempPrice * tempAmt
-		fmt.Printf("%s: %.3f, $%.2f\n", tempCur, tempAmt, tempPrice*tempAmt)
-	}
-    fmt.Println("")
-    fmt.Printf("Total: $%.2f\n", sum)
-    fmt.Println("---------------")
-}
-
-func removeCrypto(db *sql.DB, currency string) {
-	tx, _ := db.Begin()
-	statement, _ := tx.Prepare("DELETE FROM Portfolio WHERE Currency=?")
-	_, err := statement.Exec(currency)
-	checkErr(err)
-	tx.Commit()
-}
-
-func updateCrypto(db *sql.DB, currency string, amount float64) {
-	tx, _ := db.Begin()
-	statement, _ := tx.Prepare("UPDATE Portfolio set Amount=? where Currency=?")
-	_, err := statement.Exec(amount, currency)
-	checkErr(err)
-	tx.Commit()
-}
-
-func loop(db *sql.DB) {
-	var input string = "1"
-	fmt.Println("Welcome to Kripto!\n")
-	options()
-	fmt.Scanln(&input)
-	for input != "0" {
-		fmt.Println("")
-		switch input {
-		case "1":
-			fmt.Println("Here is your current portfolio:", "\n---------------")
-			getPortfolio(db)
-			fmt.Println("")
-		case "2":
-			tempInput1, tempInput2 := dualInput("Enter the crypto you want to add: ", "Enter the amount you have: ")
-			addCrypto(db, tempInput1, tempInput2)
-		case "3":
-			tempInput1, tempInput2 := dualInput("Enter the crypto you want to update: ", "Enter the new amount: ")
-			updateCrypto(db, tempInput1, tempInput2)
-		case "4":
-			fmt.Print("Enter the crypto you want to remove: ")
-			var tempInput1 string
-			fmt.Scanln(&tempInput1)
-			removeCrypto(db, tempInput1)
-		}
-		options()
-		fmt.Scanln(&input)
-	}
-	fmt.Println("Come Again!")
-}
-
-func options() {
-	fmt.Print("What would you like to do?\n",
-		"0: Exit\n",
-		"1: View Portfolio\n",
-		"2: Add Crypto\n",
-		"3: Update Crypto\n",
-		"4: Remove Crypto\n\n",
-		"Enter Choice: ")
-}
-
-func dualInput(prompt1, prompt2 string) (string, float64) {
-	var input1 string
-	var input2 string
-	fmt.Print(prompt1)
-	fmt.Scanln(&input1)
-	fmt.Print("\n", prompt2)
-	fmt.Scanln(&input2)
-	floatInput, _ := strconv.ParseFloat(input2, 64)
-	return input1, floatInput
+    return false
 }
 
 func getPrice(currency string) float64 {
     currency = strings.ToLower(currency)
 	url := "https://api.coingecko.com/api/v3/simple/price?ids=" + currency + "&vs_currencies=usd"
 	req, err := http.NewRequest("GET", url, nil)
-	checkErr(err)
+	if isErr(err) {
+        fmt.Println("err")
+    }
 	req.Header.Set("Accept", "application/json")
 
 	resp, err := http.DefaultClient.Do(req)
-	checkErr(err)
+	if isErr(err) {
+        fmt.Println("err")
+    }
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
 
